63b08f713f91820764a53258ceefd8a44535d7b1
CAM <34009133+CMichael82@users.noreply.github.com>
2018-08-06 20:21:16 -0600

    moved the letters guessed html update to after the letter comparison loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var word = wordBank[Math.floor(Math.random()*wordBank.length)];
 var letterSpaces = [];
 	for (var i = 0; i < word.length; i++) {
 		letterSpaces[i] ='_ ';
-			}
+	}
 var guessedLetters = [];
 var lettersRemaining = word.length;
 var wins = 0;
@@ -11,11 +11,9 @@ var winMessage = "You Win!"
 
 document.onkeyup = function (event){
 
-//is this the correct way to assign the user guess - seems to be duplicating keystrokes?//
+
 var userGuess = event.key;
 console.log(userGuess);
-document.getElementById("wordBlock").innerHTML= letterSpaces.join("");
-//how do you get all user guesses to show//
 document.getElementById("guessed").innerHTML=userGuess;
 console.log(word);
 
@@ -27,5 +25,6 @@ if (lettersRemaining > 0) {
 		}
 	}
 } 
+document.getElementById("wordBlock").innerHTML= letterSpaces.join("");
 
 }
