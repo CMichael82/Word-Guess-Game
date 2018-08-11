a7751f1a4d84ce9af4a9a7b3b9093fa90a5b9bbd
CAM <34009133+CMichael82@users.noreply.github.com>
2018-08-10 22:19:15 -0600

    added resetGame function, adjusted column/row layout in html, reset background color and image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,19 @@
 //Create an array of available words
-var wordBank = ["fibula", "elbow", "clavicle","cranium", "femur","vertebra", "mandible"];
+var wordBank = ["fibula", "elbow", "clavicle", "cranium", "femur", "vertebra", "mandible"];
 
 //Pick a random word from the arrray
-var word = wordBank[Math.floor(Math.random()*wordBank.length)];
+var word = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 //Create underscores to represent letter spaces based on random word
 var letterSpaces = [];
-	for (var i = 0; i < word.length; i++) {
-		letterSpaces[i] ='_ ';
-	}
+for (var i = 0; i < word.length; i++) {
+	letterSpaces[i] = '_ ';
+}
 
 //Create an empty array for letters already guessed
 var guessedLetters = [];
 
-//Determine the length of the random word
+//Determine the length of the random word 
 var lettersRemaining = word.length;
 
 //Set number of available guesses
@@ -22,52 +22,70 @@ var guessesLeft = 10;
 //Set wins starting at 0
 var wins = 0;
 var winMessage = "You Win!"
-var loseMessage ="Try Again!"
+var losses = 0;
+var lossMessage = "Try Again!"
 
+document.getElementById("wordBlock").innerHTML = letterSpaces.join("");
 //Start game with press(up)of any key
-document.onkeyup = function (event){
+document.onkeyup = function (event) {
 
-//Set the user guess to the key selected
-userGuess = event.key;
-console.log(userGuess);
-console.log(word);
+	//Set the user guess to the key selected
+	var userGuess = event.key;
+	console.log(userGuess);
+	console.log(word);
 
-// If the user guess is not in the word, move to guessed letters array
-if (word.indexOf(userGuess) > -1) {
-	} else {
+	// If the user guess is in the word, replace space with letter, reduce letters remaining,
+	if (word.indexOf(userGuess) > -1) {
+		for (var j = 0; j < 26; j++) {
+			if (word[j] === userGuess) {
+				letterSpaces[j] = userGuess;
+				lettersRemaining--;
+			}
+		}
+	}
+	// If the user guess is not in the word, move to guessed letters array
+	else {
 		guessedLetters.push(userGuess);
-
-//Decrease guesses left each time incorrect letter selected
 		guessesLeft--;
 		console.log(guessesLeft);
 	}
 
-//Check the user guess against the letters of the word. If correct reduces letters remaining
-if (lettersRemaining > 0) {
-	for (var j = 0; j < word.length; j++) {
-		if (word[j]===userGuess){
-			letterSpaces[j] = userGuess;
-			lettersRemaining--;
-		}
-	}
+	checkWin();
+	checkLoss();
 
-	//run function to see if the user won
-	checkWin ();
-}
-
-//Display stats in html
-document.getElementById("wordBlock").innerHTML= letterSpaces.join("");
-document.getElementById("guessed").innerHTML= guessedLetters.join("");
-document.getElementById("remaining").innerHTML=guessesLeft;
-document.getElementById("wins").innerHTML=wins;
+	//Display stats in html
+	document.getElementById("wordBlock").innerHTML = letterSpaces.join("");
+	document.getElementById("guessed").innerHTML = guessedLetters.join("");
+	document.getElementById("remaining").innerHTML = "Guesses Remaining:  " + guessesLeft;
+	document.getElementById("wins").innerHTML = "Wins:  " + wins;
+	document.getElementById("losses").innerHTML ="Losses:  " + losses;
 }
 
 //functions
-function checkWin (){
-	if (lettersRemaining===-0){
+function checkWin() {
+	if (lettersRemaining === 0) {
 		wins++;
-		}
+		resetGame();
+	}
 }
 
+function checkLoss() {
+	if (guessesLeft === 0) {
+		losses++;
+		// document.getElementById("skeleton").innerHTML= lossMessage;
+		resetGame();
+	}
+}
+
+function resetGame() {
+	word = wordBank[Math.floor(Math.random() * wordBank.length)];
+	letterSpaces = [];
+	for (var i = 0; i < word.length; i++) {
+		letterSpaces[i] = '_ ';
+	}
+	lettersRemaining = word.length;
+	guessedLetters = [];
+	guessesLeft = 10;
+}
 
 
